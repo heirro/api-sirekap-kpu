b2e97f07991ced786d7d78dd25874bb184b27c41
Vava Heirro <vavaheirro@live.com>
2024-02-15 01:28:30 +0700

    feat: add clarification about progress

--- src/pilpres/result.js
+++ src/pilpres/result.js
@@ -10,21 +10,22 @@ export async function routes(fastify, options) {
                     success: true,
                     data: {
                         timestamp: response.data.ts,
-                            paslon: {
-                                1: {
-                                    name: 'H. ANIES RASYID BASWEDAN, Ph.D. - Dr. (H.C.) H. A. MUHAIMIN ISKANDAR',
-                                    voters: response.data.chart['100025']
-                                },
-                                2: {
-                                    name: 'H. PRABOWO SUBIANTO - GIBRAN RAKABUMING RAKA',
-                                    voters: response.data.chart['100026']
-                                },
-                                3: {
-                                    name: 'H. GANJAR PRANOWO, S.H., M.I.P. - Prof. Dr. H. M. MAHFUD MD',
-                                    voters: response.data.chart['100027']
-                                }
+                        paslon: {
+                            1: {
+                                name: 'H. ANIES RASYID BASWEDAN, Ph.D. - Dr. (H.C.) H. A. MUHAIMIN ISKANDAR',
+                                voters: response.data.chart['100025']
                             },
+                            2: {
+                                name: 'H. PRABOWO SUBIANTO - GIBRAN RAKABUMING RAKA',
+                                voters: response.data.chart['100026']
+                            },
+                            3: {
+                                name: 'H. GANJAR PRANOWO, S.H., M.I.P. - Prof. Dr. H. M. MAHFUD MD',
+                                voters: response.data.chart['100027']
+                            }
+                        },
                         progress: {
+                            remark: "Apa ini? current dan total yang menunjukkan jumlah TPS yang sudah input real data dan total TPS yang ada.",
                             current: response.data.progres['progres'],
                             total: response.data.progres['total'],
                             percentage: response.data.chart['persen']
@@ -34,6 +35,6 @@ export async function routes(fastify, options) {
             })
             .catch(error => {
                 reply.send(error)
-            })   
+            })
     })
 }
