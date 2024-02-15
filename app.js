1ac0dced3af5c22ec1e13a91c1f8dbd56b840fef
Vava Heirro <vavaheirro@live.com>
2024-02-15 13:06:26 +0700

    fix: re-route endpoint

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import Fastify from 'fastify'
-import { routes } from './src/pilpres/result.js'
+import { result } from './src/pilpres/result.js'
+import { province } from './src/pilpres/province.js'
+
 
 const fastify = Fastify({
-  logger: false
+  logger: true
 })
 
 fastify.get('/loaderio-88f18b6967d301353c4b45fbf3a5e09e.txt', async (request, reply) => {
   reply.send('loaderio-88f18b6967d301353c4b45fbf3a5e09e')
 })
 
-fastify.register(routes)
+fastify.register(province)
+fastify.register(result)
 
 /**
  * Run the server!
